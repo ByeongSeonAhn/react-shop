890887162992c46811fcb652a761da892a5ded6d
ByeongSeonAhn <thevalueofdata@gmail.com>
2023-04-19 00:25:34 +0900

    Rating CSS 적용

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -13,16 +13,31 @@ const Rating = ({rate,count} : Rating): JSX.Element => {
         <div className='flex items-center mt-3'>
             <div className='rating rating-half'>
                 {stars.map(($elm, index) => {
-                    console.log("$el: ", $elm)
-                    console.log("index: ", index)
-                   return <input type='radio' name='rating-10' key={`rating${index}`} className='bg-yellow-400 cursor' />
+                    let cls = ""
+
+                    if(index + 1 < rate*2) { //yellow 적용
+                        if(index%2 == 0){
+                            cls = "bg-yellow-400 cursor-default mask mask-star-2 mask-half-1"
+                        } else {
+                            cls = "bg-yellow-400 cursor-default mask mask-star-2 mask-half-2"
+                        }
+                    } else { //rating 보다 작은경우 bg-yellow-400 제거
+                        if(index%2 == 0){
+                            cls = "cursor-default mask mask-star-2 mask-half-1"
+                        } else {
+                            cls = "cursor-default mask mask-star-2 mask-half-2"
+                        }
+                    }
+
+
+                   return <input type='radio' name='rating-10' key={`rating${index}`} className={cls} />
                 })}
             </div>
             <div className='ml-2'>
                 {rate} / {count} 참여
             </div>
         </div>
-    );
+    )
 }
 
 Rating.defaultProps = defaultProps;
